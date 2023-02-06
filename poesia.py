eb1d602156c566b739bea140e7d74a4fe7441d33
Patricia Angelini <profPatricia.Angelini@fiap.com.br>
2023-02-06 10:38:09 -0300

    Acrescentamos o arquivo poesia.py

---
+++ poesia.py
@@ -0,0 +1,6 @@
+#poesia
+print("De tudo, ao meu amor serei atento")
+print("Antes, e com tal zelo, e sempre, e tanto")
+print("Que mesmo em face do maior encanto")
+print("Dele se encante mais meu pensamento!")
+
