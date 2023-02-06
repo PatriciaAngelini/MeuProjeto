46e8fd2625f60412264916087900d7788f095232
Patricia Angelini <profPatricia.Angelini@fiap.com.br>
2023-02-06 15:32:12 -0300

    .

--- poesia.py
+++ poesia.py
@@ -1,4 +1,4 @@
-#poesia Soneto
+#poesia Soneto de Fidelidade
 print("De tudo, ao meu amor serei atento")
 print("Antes, e com tal zelo, e sempre, e tanto")
 print("Que mesmo em face do maior encanto")
