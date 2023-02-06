2f4df12109d8d8f9be8f78eb89ef49d3a89bfda5
PatriciaAngelini <62622184+PatriciaAngelini@users.noreply.github.com>
2023-02-06 16:58:04 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,9 @@
+titulo = "Media de dois valores"
+print(f'{titulo:^30}')
+
+x = float(input('Entre com o valor de x: '))
+y = float(input('Entre com o valor de y: '))
+media = (x+y)/2
+print("O valor de x é", x, "!")
+print('O valor de y é', y)
+print('A media dos valores é', round(media, 3))
