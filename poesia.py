6bffc8ec80ca690e12d7efce60906a4c26ddc4f8
PatriciaAngelini <62622184+PatriciaAngelini@users.noreply.github.com>
2023-02-06 15:13:13 -0300

    Update poesia.py

--- poesia.py
+++ poesia.py
@@ -1,4 +1,4 @@
-#poesia Soneto de Felicidade
+#poesia Soneto
 print("De tudo, ao meu amor serei atento")
 print("Antes, e com tal zelo, e sempre, e tanto")
 print("Que mesmo em face do maior encanto")
