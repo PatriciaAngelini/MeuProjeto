59fc6732445b139c5216b99f5ac663b897eb4c52
PatriciaAngelini <62622184+PatriciaAngelini@users.noreply.github.com>
2023-02-06 16:43:59 -0300

    Update poesia.py

--- poesia.py
+++ poesia.py
@@ -3,4 +3,4 @@ print("De tudo, ao meu amor serei atento")
 print("Antes, e com tal zelo, e sempre, e tanto")
 print("Que mesmo em face do maior encanto")
 print("Dele se encante mais meu pensamento!")
-
+print("\n\n")
