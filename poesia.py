05445be4dbd5945a79f342b0e40cb21bc790e18e
PatriciaAngelini <62622184+PatriciaAngelini@users.noreply.github.com>
2023-02-06 17:12:23 -0300

    Update poesia.py

--- poesia.py
+++ poesia.py
@@ -1,6 +1,13 @@
-#poesia Soneto de Fidelidade.
+#poesia Soneto de Fidelidade de Vinicius de Moraes
 print("De tudo, ao meu amor serei atento")
-print("Antes, e com tal zelo, e sempre, e tanto")
+print("Antes, e com tal zelo, e sempre, e tanto......")
 print("Que mesmo em face do maior encanto")
 print("Dele se encante mais meu pensamento!")
-print("\n\n")
+print("\n\n\n")
+
+print("Quero vivê-lo em cada vão momento.")
+print("E em seu louvor hei de espalhar meu canto.")
+print("E rir meu riso e derramar meu pranto.")
+print("Ao seu pesar ou seu contentamento")
+print("\n\n\n")
+
